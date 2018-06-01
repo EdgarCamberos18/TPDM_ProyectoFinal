df53f22b01f5a97f1f6f598cfb8f5b332bfd0ace
EdgarCamberos18 <edulcamberosar@ittepic.edu.mx>
2018-06-01 01:53:18 -0600

    Configuraciones Firebase

---
+++ app/src/main/java/mx/edu/ittepic/tpdm_proyectofinal/Connection.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.tpdm_proyectofinal;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class Connection {
+    private DatabaseReference mDatabase;
+
+    private void addNewBabySister(int numeroCelular,String nombre,String appellidoPaterno,
+                                  String apellidoMaterno,String domicilio, int edad,
+                                  String perfil,int tipoServicio){
+
+    }
+}
